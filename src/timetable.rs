1dd62680e57142239ff92018adfcfca7f9c68445
pshkqs <pashamail79@gmail.com>
2022-12-06 13:57:22 +0300

    hash of day

--- src/timetable.rs
+++ src/timetable.rs
@@ -1,17 +1,22 @@
 use chrono::{DateTime, Utc};
-use sha2::{digest::Digest, Sha512};
+use sha2::{digest::Digest, Sha256, Sha512};
 
+use crate::utils;
+
+#[derive(Debug)]
 pub struct Group {
   pub name: String,
   pub lessons: Vec<Lesson>,
 }
 
+#[derive(Debug)]
 pub struct Lesson {
   pub num: usize,
   pub name: String,
   pub classroom: String,
 }
 
+#[derive(Debug)]
 pub struct Day {
   pub date: DateTime<Utc>,
   pub hash: String,
@@ -19,30 +24,21 @@ pub struct Day {
 }
 
 impl Day {
-  pub fn new(date: Option<DateTime<Utc>>, groups: Vec<Group>) -> Day {
+  /// date is None = today.
+  pub fn new(groups: Vec<Group>, date: Option<DateTime<Utc>>) -> Self {
     let date = date.unwrap_or(chrono::Utc::now());
-    let hash = Day::get_hash(&groups);
-    Day { date, hash, groups }
+    let hash = Self::get_hash(&groups);
+    Self { date, hash, groups }
   }
 
   fn get_hash(groups: &Vec<Group>) -> String {
-    fn to_bytes<'a>(n: usize) -> [u8; 8] {
-      let mut res = [0u8; (usize::BITS / 8) as usize];
-      let mut n = n;
-      for i in 0..(usize::BITS / 8) as usize {
-        res[i] = ((n >> (8 * i)) & 0xff) as u8;
-        n = n >> i;
-      }
-      res
-    }
-
-    let mut hasher = Sha512::new();
+    let mut hasher = Sha256::new();
     for group in groups {
       hasher.update(&group.name);
       for lesson in &group.lessons {
         hasher.update(&lesson.classroom);
         hasher.update(&lesson.name);
-        hasher.update(to_bytes(lesson.num));
+        hasher.update(utils::usize_as_bytes(lesson.num));
       }
     }
 
