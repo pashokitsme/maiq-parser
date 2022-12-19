f3ea03c5f283e480be30ee5b760f93da29b5d7bc
pshkqs <pashamail79@gmail.com>
2022-12-19 18:50:58 +0300

    count of lessons -> their clones

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,48 @@
-use maiq_parser::{
-  fetch::{fetch, Fetch},
-  parser::parse,
-  timetable::{Day, LessonKind},
-};
+// use maiq_parser::{
+//   fetch::{fetch, Fetch},
+//   parser::parse,
+//   timetable::{Day, LessonKind},
+// };
 
-#[tokio::main]
-async fn main() {
-  dotenvy::dotenv().unwrap();
-  let fetched = fetch(Fetch::Tomorrow).await.unwrap();
-  println!("{}", fetched);
-  let day = match parse(&fetched).await {
-    Ok(x) => x,
-    Err(x) => return println!("Ошибка: {}", x),
-  };
+// #[tokio::main]
+// async fn main() {
+//   dotenvy::dotenv().unwrap();
+//   let fetched = fetch(Fetch::Tomorrow).await.unwrap();
+//   println!("{}", fetched);
+//   let day = match parse(&fetched).await {
+//     Ok(x) => x,
+//     Err(x) => return println!("Ошибка: {}", x),
+//   };
 
-  print_day(&day)
-}
+//   print_day(&day)
+// }
 
-fn print_day(day: &Day) {
-  println!("{} от {}\n", day.uid, day.date);
-  for group in &day.groups {
-    println!("Группа {}", group.name);
-    for lesson in &group.lessons {
-      print!("\t");
-      match lesson.kind {
-        LessonKind::None => println!("#{} Нет", lesson.num),
-        LessonKind::Default => println!("#{} По расписанию", lesson.num),
-        LessonKind::Some => {
-          if let Some(sub) = lesson.subgroup {
-            print!("Подгруппа {} ", sub)
-          }
-          println!(
-            "#{} {} в {}. Преподаватель {}",
-            lesson.num,
-            lesson.name.as_ref().unwrap(),
-            lesson.classroom.as_ref().unwrap_or(&"-".to_string()),
-            lesson.teacher.as_ref().unwrap_or(&"-".to_string())
-          )
-        }
-      }
-    }
+// fn print_day(day: &Day) {
+//   println!("{} от {}\n", day.uid, day.date);
+//   for group in &day.groups {
+//     println!("Группа {}", group.name);
+//     for lesson in &group.lessons {
+//       print!("\t");
+//       match lesson.kind {
+//         LessonKind::None => println!("#{} Нет", lesson.num),
+//         LessonKind::Default => println!("#{} По расписанию", lesson.num),
+//         LessonKind::Some => {
+//           if let Some(sub) = lesson.subgroup {
+//             print!("Подгруппа {} ", sub)
+//           }
+//           println!(
+//             "#{} {} в {}. Преподаватель {}",
+//             lesson.num,
+//             lesson.name.as_ref().unwrap(),
+//             lesson.classroom.as_ref().unwrap_or(&"-".to_string()),
+//             lesson.teacher.as_ref().unwrap_or(&"-".to_string())
+//           )
+//         }
+//       }
+//     }
 
-    println!()
-  }
-}
+//     println!()
+//   }
+// }
 
-// fn main() {}
+fn main() {}
