80faf692ec72a5574dff077162c910611ca6b36e
pashokitsme <pashamail79@gmail.com>
2022-12-19 11:28:39 +0300

    Fixed: panic on double field skip in timetable

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,12 @@
+use std::num::ParseIntError;
+
 #[derive(thiserror::Error, Debug)]
 pub enum ParserError {
   #[error("Расписания ещё нет")]
   NotYet,
 
-  #[error("Не удалось спарсить")]
-  CantParse,
+  #[error("Не удалось спарсить: {0}")]
+  CantParse(String),
 
   #[error("{0}")]
   NetworkError(reqwest::Error),
@@ -13,5 +15,11 @@ pub enum ParserError {
 impl From<reqwest::Error> for ParserError {
   fn from(err: reqwest::Error) -> Self {
     ParserError::NetworkError(err)
+  }
+}
+
+impl From<ParseIntError> for ParserError {
+  fn from(err: ParseIntError) -> Self {
+    ParserError::CantParse(err.to_string())
   }
 }
