921e0c3f61e1e4453d20530d1bc5a0736cb7fc5c
pashokitsme <pashamail79@gmail.com>
2022-12-21 03:06:38 +0300

    Error messages

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,43 @@
-use maiq_parser::{
-  fetch::{fetch, Fetch},
-  parser::parse,
-  timetable::Snapshot,
-};
+// use maiq_parser::{
+//   fetch::{fetch, Fetch},
+//   parser::parse,
+//   timetable::Snapshot,
+// };
 
-#[tokio::main]
-async fn main() {
-  dotenvy::dotenv().unwrap();
-  let fetched = fetch(Fetch::Today).await.unwrap();
-  println!("{}", fetched);
-  let s = match parse(&fetched).await {
-    Ok(x) => x,
-    Err(x) => return println!("Ошибка: {}", x),
-  };
+// #[tokio::main]
+// async fn main() {
+//   dotenvy::dotenv().unwrap();
+//   let fetched = fetch(Fetch::Today).await.unwrap();
+//   println!("{}", fetched);
+//   let s = match parse(&fetched).await {
+//     Ok(x) => x,
+//     Err(x) => return println!("Ошибка: {}", x),
+//   };
 
-  print_snapshot(&s)
-}
+//   print_snapshot(&s)
+// }
 
-fn print_snapshot(s: &Snapshot) {
-  println!("{} от {}\n", s.uid, s.date);
-  for group in &s.groups {
-    println!("Группа {}", group.name);
-    for lesson in &group.lessons {
-      print!("\t#{} ", lesson.num);
-      if let Some(sub) = lesson.subgroup {
-        print!(" (п. {}) ", sub)
-      }
-      print!("{} ", lesson.name);
+// fn print_snapshot(s: &Snapshot) {
+//   println!("{} от {}\n", s.uid, s.date);
+//   for group in &s.groups {
+//     println!("Группа {}", group.name);
+//     for lesson in &group.lessons {
+//       print!("\t#{} ", lesson.num);
+//       if let Some(sub) = lesson.subgroup {
+//         print!(" (п. {}) ", sub)
+//       }
+//       print!("{} ", lesson.name);
 
-      if let Some(classroom) = lesson.classroom.as_ref() {
-        print!("в {}", classroom);
-      }
+//       if let Some(classroom) = lesson.classroom.as_ref() {
+//         print!("в {}", classroom);
+//       }
 
-      if let Some(teacher) = lesson.teacher.as_ref() {
-        print!(". Преподаватель: {}", teacher)
-      }
-      println!()
-    }
-    println!()
-  }
-}
-// fn main() {}
+//       if let Some(teacher) = lesson.teacher.as_ref() {
+//         print!(". Преподаватель: {}", teacher)
+//       }
+//       println!()
+//     }
+//     println!()
+//   }
+// }
+fn main() {}
