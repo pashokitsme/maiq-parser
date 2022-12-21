12b082f357ada665a72f493667baf169f81db15b
pshkqs <pashamail79@gmail.com>
2022-12-21 06:38:16 +0300

    Date parsing

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,47 @@
-// use maiq_parser::{
-//   fetch::{fetch, Fetch},
-//   parser::parse,
-//   timetable::Snapshot,
-// };
+use maiq_parser::{
+  fetch::{fetch, Fetch},
+  parser::parse,
+  timetable::Snapshot,
+};
 
-// #[tokio::main]
-// async fn main() {
-//   dotenvy::dotenv().unwrap();
-//   let fetched = fetch(Fetch::Today).await.unwrap();
-//   println!("{}", fetched);
-//   let s = match parse(&fetched).await {
-//     Ok(x) => x,
-//     Err(x) => return println!("Ошибка: {}", x),
-//   };
+// ? Too lazy to write tests
 
-//   print_snapshot(&s)
-// }
+#[allow(dead_code, unused_variables)]
+#[tokio::main]
+async fn main() {
+  dotenvy::dotenv().unwrap();
+  let fetched = fetch(Fetch::Tomorrow).await.unwrap();
+  println!("{}", fetched);
+  let s = match parse(&fetched).await {
+    Ok(x) => x,
+    Err(x) => return println!("Ошибка: {}", x),
+  };
 
-// fn print_snapshot(s: &Snapshot) {
-//   println!("{} от {}\n", s.uid, s.date);
-//   for group in &s.groups {
-//     println!("Группа {}", group.name);
-//     for lesson in &group.lessons {
-//       print!("\t#{} ", lesson.num);
-//       if let Some(sub) = lesson.subgroup {
-//         print!(" (п. {}) ", sub)
-//       }
-//       print!("{} ", lesson.name);
+  // print_snapshot(&s)
+  // println!("Date: {}", s.date)
+}
 
-//       if let Some(classroom) = lesson.classroom.as_ref() {
-//         print!("в {}", classroom);
-//       }
+#[allow(dead_code)]
+fn print_snapshot(s: &Snapshot) {
+  println!("{} от {}\n", s.uid, s.date);
+  for group in &s.groups {
+    println!("Группа {}", group.name);
+    for lesson in &group.lessons {
+      print!("\t#{} ", lesson.num);
+      if let Some(sub) = lesson.subgroup {
+        print!(" (п. {}) ", sub)
+      }
+      print!("{} ", lesson.name);
 
-//       if let Some(teacher) = lesson.teacher.as_ref() {
-//         print!(". Преподаватель: {}", teacher)
-//       }
-//       println!()
-//     }
-//     println!()
-//   }
-// }
-fn main() {}
+      if let Some(classroom) = lesson.classroom.as_ref() {
+        print!("в {}", classroom);
+      }
+
+      if let Some(teacher) = lesson.teacher.as_ref() {
+        print!(". Преподаватель: {}", teacher)
+      }
+      println!()
+    }
+    println!()
+  }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use chrono::{Datelike, Days, NaiveDate, Weekday};
+
 const SIZE_OF_USIZE: usize = (usize::BITS / 8) as usize;
 
 pub fn usize_as_bytes(n: usize) -> [u8; SIZE_OF_USIZE] {
@@ -8,4 +10,28 @@ pub fn usize_as_bytes(n: usize) -> [u8; SIZE_OF_USIZE] {
     n = n >> i;
   }
   res
+}
+
+pub fn map_weekday<'d>(day: Weekday) -> &'d str {
+  match day {
+    Weekday::Mon => "Понедельник",
+    Weekday::Tue => "Вторник",
+    Weekday::Wed => "Среда",
+    Weekday::Thu => "Четверг",
+    Weekday::Fri => "Пятница",
+    Weekday::Sat => "Суббота",
+    Weekday::Sun => "Воскресенье",
+  }
+}
+
+pub fn map_day<'d>(date: &NaiveDate, day: &'d str) -> NaiveDate {
+  let mut max = 10;
+  let count = date
+    .iter_days()
+    .take_while(|x| {
+      max -= 1;
+      map_weekday(x.weekday()) == day || max == 0
+    })
+    .count();
+  date.checked_add_days(Days::new(count as u64)).unwrap()
 }
